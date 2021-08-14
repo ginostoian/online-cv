b7c0161c8d92ec9ee49abc2d9271738b2fbf0d32
Gino Stoian <ginostoian@gmail.com>
2021-08-14 03:17:51 +0300

    add link action function to remove menu on click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 // Show Menu
-
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId)
     const nav = document.getElementById(navId)
@@ -13,3 +12,14 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle', 'nav-menu')
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(link => link.addEventListener('click', linkAction))
